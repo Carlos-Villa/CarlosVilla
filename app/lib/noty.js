85cb9cdd30276377b1db9eaca0b12b5330019591
Carlos Villa <ckoryones@live.com.mx>
2019-06-24 22:42:26 -0500

    permissions noty

--- app/lib/noty.js
+++ app/lib/noty.js
@@ -2,13 +2,28 @@ let noty = class Noty{
 
 	check()
 	{
+		
 		if (window.Notification && Notification.permission !== "granted") {
 			Notification.requestPermission(function (status) {
 				if (Notification.permission !== status) {
 					Notification.permission = status;
 				}
-				var n = new Notification("Holiwis! " , {tag: 'soManyNotification'});
+				let data = {title:'test'};
+				let opts = {
+					body:`<h1>${data.title}</h1>`,
+					icon:''
+				}
+				new Notification("Hi " , opts);
 			});
+		}
+		else if (window.Notification && Notification.permission === "granted")
+		{
+			let data = {title:'test'};
+			let opts = {
+				body:`<h1>${data.title}</h1>`,
+				icon:''
+			}
+			new Notification("Hi " , opts);
 		}
 	}
 }
